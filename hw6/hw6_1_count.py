dd77fa1e7fbcea08a454942e98361b3fe197975c
arinablake <arinafilippova@gmail.com>
2020-07-22 18:40:08 -0400

    homework 6 algorithms updated

--- hw6/hw6_1_count.py
+++ hw6/hw6_1_count.py
@@ -3,24 +3,25 @@
 # 1)count how many elements are in the array
 
 
-def count_numbers_in_list(lst):
+def array(lst):
     if lst == []:
         return 0
     else:
-        return 1 + count_numbers_in_list(lst[1:])
+        return 1 + array(lst[1:])
+
+print(array([1, 2, 2, 2, 2, 3, 4, 7, 8, 8]))
 
 
 # 2)count how many times each element occurs in the array
-print(count_numbers_in_list([1, 2, 2, 2, 2, 3, 4, 7, 8, 8]))
 
 
-def count(f, s):
-    if s == []:
-        return 0
-    elif f == s[0]:
-        return 1 + count(f, s[1:])
-    else:
-        return 0 + count(f, s[1:])
-
-
-print(count(2, [1, 2, 2, 2, 2, 3, 4, 7, 4, 8, 8]))
+# def count(f, s):
+#     if s == []:
+#         return 0
+#     elif f == s[0]:
+#         return 1 + count(f, s[1:])
+#     else:
+#         return 0 + count(f, s[1:])
+#
+#
+# print(count(2, [1, 2, 2, 2, 2, 3, 4, 7, 4, 8, 8]))
--- hw6/hw6_2_max.py
+++ hw6/hw6_2_max.py
@@ -1,18 +1,35 @@
 # Find the biggest number in the list (divide and rule)
 
 
-def find_max(lst, left, right):
-    if left == right:
-        return lst[left]
+# def find_max(lst, left, right):
+#     if left == right:
+#         return lst[left]
+#     else:
+#         mid = int((left + right) / 2)
+#         max1 = find_max(lst, left, mid)
+#         max2 = find_max(lst, mid + 1, right)
+#         if max1 > max2:
+#             return max1
+#         else:
+#             return max2
+#
+#
+# lst = [2, 5, 4, 9, 3, 17, 5, 1, 10, 6]
+# print('Maximum element of the list is', find_max(lst, 0, len(lst) - 1))
+
+
+def biggest(array):
+    if len(array) == 0:
+        return 0
+    if len(array) == 1:
+        return array[0]
+    mid = len(array) // 2
+    max1 = biggest(array[:mid])
+    max2 = biggest(array[mid:])
+    if max1 > max2:
+        return max1
     else:
-        mid = int((left + right) / 2)
-        max1 = find_max(lst, left, mid)
-        max2 = find_max(lst, mid + 1, right)
-        if max1 > max2:
-            return max1
-        else:
-            return max2
+        return max2
 
 
-lst = [2, 5, 4, 9, 3, 17, 5, 1, 10, 6]
-print('Maximum element of the list is', find_max(lst, 0, len(lst) - 1))
+print('Maximum element of the list is', biggest([2, 5, 4, 11, 9, 3, 17, 5, 19, 10, 6]))
